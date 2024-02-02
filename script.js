ae5c2136e19bb1b6f80162afafc464eabf24139d
opitz21254 <113393776+opitz21254@users.noreply.github.com>
2024-02-02 08:53:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ window.onload = function() {
     let dropdownMenu = document.querySelector("#w-dropdown-list-1 > div");
     dropdownMenu.style.display = 'block';
 };
+
+
+// Get the elements
+let exploreTab = document.querySelector('global-nav__dropdown w-dropdown-toggle');
+let dropdownMenu = document.querySelector('#w-dropdown-list-1 > div');
+dropdownMenu.style.display = 'none'; // Initially hide the dropdown
+
+// Add event listeners
+exploreTab.addEventListener('mouseover', function() {
+  dropdownMenu.style.display = 'block';
+});
+
+exploreTab.addEventListener('mouseout', function() {
+  dropdownMenu.style.display = 'none';
+});
